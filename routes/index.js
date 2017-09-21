58e1e6af62f60142308c24b32e4d32ecb35eeee3
tallroids <=>
2017-09-21 12:58:47 -0600

    updated package

--- routes/index.js
+++ routes/index.js
@@ -2,20 +2,27 @@ var express = require('express');
 var router = express.Router();
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', {
-    title: 'Nothing Here'
+  console.log("Confirmation page")
+  res.render('test', {
+    name: "Error! Try again Buster"
   });
 });
 
 /* POST home page. */
 router.post('/', function (req, res, next) {
-  console.log(req.lti)
-  console.log(req.session)
   var ltiInfo = req.lti.body
   res.render('index', {
     name: ltiInfo.lis_person_name_given,
     course: ltiInfo.context_title,
     info: ltiInfo
+  });
+});
+
+/* POST home page. */
+router.post('/pathtocontent', function (req, res, next) {
+  var ltiInfo = req.lti.body
+  res.render('test', {
+    name: "Super Special Content page"
   });
 });
 
